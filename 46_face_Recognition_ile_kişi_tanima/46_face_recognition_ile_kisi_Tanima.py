1470efe4f92b4fe793205daa21768a58f91c61aa
Mustafa Serhat Özdemir <msozdemir9696@gmail.com>
2024-09-12 21:17:12 +0300

    Add files via upload

---
+++ 46_face_Recognition_ile_kişi_tanima/46_face_recognition_ile_kisi_Tanima.py
@@ -0,0 +1,42 @@
+import cv2
+import dlib
+import face_recognition
+
+detector=dlib.get_frontal_face_detector()
+serhat=face_recognition.load_image_file("photo.jpg")
+serhat_enc=face_recognition.face_encodings(serhat)[0]
+
+cap=cv2.VideoCapture(0)
+
+while True:
+    ret,frame=cap.read()
+
+    face_loc=[]
+    faces=detector(frame)
+    for face in faces:
+        x=face.left()
+        y=face.top()
+        w=face.right()
+        h=face.bottom()
+        face_loc.append((y,w,h,x))
+
+    #face_log=face_recognition.face_locations(frame)
+    face_encoding=face_recognition.face_encodings(frame,face_loc)
+
+    i=0
+    for face in face_encoding:
+        y, w, h, x=face_loc[i]
+        sonuc=face_recognition.compare_faces([serhat_enc],face)
+        if sonuc[0]==True:
+            cv2.rectangle(frame,(x,y),(w,h),(255,0,0),2)
+            cv2.putText(frame,"SERHAT",(x,h+35),cv2.FONT_HERSHEY_PLAIN,1,(0,0,255),1)
+        else:
+            cv2.rectangle(frame, (x, y), (w, h), (255, 0, 0), 2)
+            cv2.putText(frame, "YABANCİ", (x, h + 35), cv2.FONT_HERSHEY_PLAIN, 1, (0, 0, 255), 1)
+    cv2.imshow("KAMERA",frame)
+
+    if cv2.waitKey(10) & 0xFF==ord("q"):
+        break
+
+cap.release()
+cv2.destroyAllWindows()
